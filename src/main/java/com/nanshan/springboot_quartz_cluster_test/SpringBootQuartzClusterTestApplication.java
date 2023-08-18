d30440acb37b6cead865d40136c74ffcd55472ab
Roger Lo <sobadrush@icloud.com>
2023-08-18 20:18:42 +0800

    init commit

---
+++ src/main/java/com/nanshan/springboot_quartz_cluster_test/SpringBootQuartzClusterTestApplication.java
@@ -0,0 +1,13 @@
+package com.nanshan.springboot_quartz_cluster_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuartzClusterTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootQuartzClusterTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nanshan/springboot_quartz_cluster_test/SpringBootQuartzClusterTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanshan.springboot_quartz_cluster_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQuartzClusterTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
